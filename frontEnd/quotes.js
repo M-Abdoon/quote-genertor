eaef7d08199d849e673b2098ea35290a22cfb1e5
Mohammed Abdoon <mohammedabdoon1996@gmail.com>
2026-05-08 18:39:02 +0100

    added quote adding feature

--- frontEnd/quotes.js
+++ frontEnd/quotes.js
@@ -1,12 +1,16 @@
+//const serverUrl = "https://m-abdoon-quote-server.hosting.codeyourfuture.io";
+const serverUrl = "http://127.0.0.1:3000";
+
 function setup() {
   const quoteButton = document.getElementById("new-quote");
   const quoteText = document.getElementById("quote");
   const authorText = document.getElementById("author");
+  const addAuthorInput = document.getElementById("add-author");
+  const addQuoteInput = document.getElementById("add-quote");
+  const addQuoteButton = document.getElementById("add-quote-button");
 
   async function getQuoteAPI() {
-    const response = await fetch(
-      "https://m-abdoon-quote-server.hosting.codeyourfuture.io/",
-    );
+    const response = await fetch(serverUrl);
     const quoteString = await response.json();
 
     return quoteString;
@@ -25,6 +29,32 @@ function setup() {
   quoteButton.addEventListener("click", showRandomQuote);
 
   showRandomQuote();
+
+  addQuoteButton.addEventListener("click", async (e) => {
+    e.preventDefault();
+    const newQuote = addQuoteInput.value;
+    const newAuthor = addAuthorInput.value;
+
+    if (newQuote && newAuthor) {
+      const response = await fetch(serverUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ quote: newQuote, author: newAuthor }),
+      });
+
+      if (response.ok) {
+        addQuoteInput.value = "";
+        addAuthorInput.value = "";
+        alert("Quote added successfully!");
+      } else {
+        alert(`Failed to add quote.${response}`);
+      }
+    } else {
+      alert("Please enter both a quote and an author.");
+    }
+  });
 }
 
 document.addEventListener("DOMContentLoaded", setup);
